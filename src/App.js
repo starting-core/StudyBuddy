906fc5426ec2acf1cb0764cca794580f4d75335e
starting-core <93027313+starting-core@users.noreply.github.com>
2021-10-23 12:04:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p
+          className="App-link"
+         
+        >
+          Study Buddy 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
